233910d5b25eb4c0bd89a2184e4f47a7819e432e
Matt <Faymatt96@gmail.com>
2017-09-26 00:51:22 -0400

    app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const get_off_facebook = true;
+while (get_off_facebook) {
+  alert('GET OFF OF FACEBOOK!!!!!');
+};
+
+
+/*** Todo: allow user to see how many times they tried accessing Facebook ***/
