94d305ef04f23927f73fc7100b0d82f5c055e867
Matt <faymatt96@gmail.com>
2017-11-01 00:26:05 -0400

    Update script

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 
 const act = document.getElementById('activate');
 const dis = document.getElementById('disable');
+const array_of_URL = [];
+
+localStorage.setItem(array_of_URL);
 
 act.addEventListener('click', runAlert);
 
 function runAlert() { 
+  // Add URL to the list.
+  //array_of_URL.push(url);  
   let blocked = true;
   while (blocked) {
     alert('Blocked on your request. Get back to work!');
@@ -14,5 +19,9 @@ function runAlert() {
 dis.addEventListenet('click', disable);
 
 function disable() {
-  return;
+  return false;
 };
+
+let url = document.getElementById('inputURL');
+
+
